a7fc2e4d1cfb7c1e611f36fb492899fe12330843
mollymoog <mmkover@gmail.com>
2020-12-04 23:43:46 -0500

    commit tree 16ae7a931dee90828af4e645dd4b6f6cf83b16bc

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+
+function mapToNegativize(sourceArray) {
+    return sourceArray.map(el => -el);
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray.map(el => el);
+}
+
+function mapToDouble(sourceArray) {
+    return sourceArray.map(el => 2 * el);
+}
+
+function mapToSquare(sourceArray) {
+    return sourceArray.map(el => el ** 2);
+}
+
+function reduceToTotal(sourceArray, startingPoint=0) { 
+    return sourceArray.reduce(((startingPoint, current) => startingPoint + current), startingPoint);
+}
+
+function reduceToAllTrue(sourceArray) {
+    return sourceArray.reduce((all, current) => {
+        if (all === false || current === false) return false;
+        else {return true};
+    });
+}
+
+function reduceToAnyTrue(sourceArray) {
+    return sourceArray.reduce((all, current) => {
+        if (current === true) return true;
+        else {return false};
+    });
+}
